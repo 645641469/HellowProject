ae793cb10f5adac51217196291f0125693e0afa5
645641469@qq.com <645641469@qq.com>
2019-04-19 22:15:47 +0800

    12313131

--- src/com/liuzhao/com/HelloWord.java
+++ src/com/liuzhao/com/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
 		System.out.println("Hello word");
 		System.out.println("Hello word");
+		System.out.println("Hello word");
 	}
 }
